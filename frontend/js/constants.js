1a1ac6115fa86d756f517199171f47e9cefa2f08
Free Ukraine <freeukrainewar@gmail.com>
2022-04-11 11:49:26 -0700

    updated site

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x3B618ADf9cF5BB645d0ccd416b50703Af5213952"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
